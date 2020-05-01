4956378972798be80490a35fb6cac72b29fcec11
Donovan van Heerden <dp.vanheerden@gmail.com>
2020-05-01 21:40:40 +0200

    renamed friend to userfriend #15

--- backend/Fanime.Domain/Entities/UserFriend.cs
+++ backend/Fanime.Domain/Entities/UserFriend.cs
@@ -2,14 +2,15 @@
 
 namespace Fanime.Domain.Entities
 {
-    public class Friends
+    public class UserFriend
     {
-        public FriendStatus Status { get; set; }
         public int UserId { get; set; }
         public User User { get; set; } // Current Person
 
         public int FriendId { get; set; }
         public User Friend { get; set; } // Current Person's friend
+
+        public FriendStatus Status { get; set; }
 
         public DateTime? Invited { get; set; }
         public DateTime? Accepted { get; set; }
