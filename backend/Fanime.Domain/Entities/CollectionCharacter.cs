c3facc0502f907e894d46a14e030ae0acf121273
Donovan van Heerden <dp.vanheerden@gmail.com>
2020-05-01 16:28:52 +0200

    Added new entity, collectionCharacter, to align with database design

---
+++ backend/Fanime.Domain/Entities/CollectionCharacter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fanime.Domain.Entities
+{
+    public class CollectionCharacter
+    {
+        public int CharacterId { get; set; }
+        public Character Character { get; set; }
+
+        public int CollectionId { get; set; }
+        public CollectionBase Collection { get; set; }
+    }
+}
